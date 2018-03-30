88f3e2248bc5cf222f974dcbc255a8d60026146d
smachtmes <smachtmes@gmail.com>
2018-03-30 14:27:18 -0700

    added groups data bag

--- roles/web_server.rb
+++ roles/web_server.rb
@@ -1,4 +1,4 @@
 name "web_server"
 description "Web Server"
-run_list 'role[base]','recipe[company_web]'
+run_list 'recipe[myusers]','role[base]','recipe[company_web]'
 default_attributes 'company_web' => {'company_name' => 'E Corp'}
